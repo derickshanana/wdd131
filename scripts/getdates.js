1818679d6412b719ed75a846124e65e419231cb0
derick2001 <derickshanana50@gmail.com>
2026-03-05 13:46:41 +0200

    created homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Dynamically set the current copyright year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = currentYear;
+
+// Dynamically set the last modified date
+document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
